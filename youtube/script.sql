3f832a6e699a57104db9d5ecfea07f241e8b8ac2
Stenio Wagner <steni.wagner1@gmail.cm>
2023-09-21 00:42:24 -0300

    youtube: creating the tables schema

---
+++ youtube/script.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE youtube;
+
+CREATE TABLE IF NOT EXISTS user_profile (
+    id BIGSERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    gender TEXT CHECK(gender IN ('MALE', 'FEMALE')) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS account (
+    id BIGSERIAL PRIMARY KEY REFERENCES user_profile(id),
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS channel (
+    id BIGSERIAL PRIMARY KEY,
+    account_id BIGINT NOT NULL REFERENCES account(id),
+    channel_name TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS channel_subscriber (
+    account_id BIGINT REFERENCES account(id),
+    channel_id BIGINT REFERENCES channel(id),
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    PRIMARY KEY (account_id, channel_id)
+);
+
+CREATE TABLE IF NOT EXISTS video (
+    id BIGSERIAL PRIMARY KEY,
+    channel_id BIGINT NOT NULL REFERENCES channel(id),
+    url TEXT UNIQUE NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT,
+    category TEXT NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS video_view (
+    account_id BIGINT NOT NULL REFERENCES account(id),
+    video_id BIGINT NOT NULL REFERENCES video(id),
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS video_comment (
+    id BIGSERIAL PRIMARY KEY,
+    account_id BIGINT NOT NULL REFERENCES account(id),
+    video_id BIGINT NOT NULL REFERENCES video(id),
+    comment TEXT NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS video_like (
+    video_id BIGINT PRIMARY KEY REFERENCES video(id),
+    account_id BIGINT REFERENCES account(id),
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS video_comment_like (
+    video_comment_id BIGINT REFERENCES video_comment(id),
+    account_id BIGINT REFERENCES account(id),
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    PRIMARY KEY (video_comment_id, account_id)
+);
